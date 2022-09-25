9679e0483d05b44713858430ac0cb154893cacfd
Dane Rieber <danerieber@gmail.com>
2022-09-25 17:00:12 -0600

    finished SQL import scripts for all tables

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name imdb-but-actually -p 5432:5432 -v $(pwd)/data:/data -v $(pwd)/pg-data:/var/lib/postgresql/data -d postgresql:latest
+docker run --name imdb-but-actually -p 5432:5432 -v $(pwd)/data:/data -v $(pwd)/pg-data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=docker -d postgres:latest
--- sql/import.sql
+++ sql/import.sql
@@ -33,3 +33,69 @@ COPY title.akas
     DELIMITER E'\t'
     NULL '\N'
     CSV HEADER;
+
+CREATE TABLE title.crew (
+    tconst              TEXT        PRIMARY KEY,
+    directors           TEXT,
+    writers             TEXT
+);
+COPY title.crew
+    FROM '/data/title.crew.tsv'
+    DELIMITER E'\t'
+    NULL '\N'
+    CSV HEADER;
+
+CREATE TABLE title.episode (
+    tconst               TEXT        PRIMARY KEY,
+    parent_tconst        TEXT,
+    season_number        INT,
+    episode_number       INT
+);
+COPY title.episode
+    FROM '/data/title.episode.tsv'
+    DELIMITER E'\t'
+    NULL '\N'
+    CSV HEADER;
+
+CREATE TABLE title.principals (
+    tconst              TEXT        NOT NULL,
+    ordering            INT         NOT NULL,
+    nconst              TEXT        NOT NULL,
+    category            TEXT,
+    job                 TEXT,
+    characters          TEXT,
+    PRIMARY KEY(tconst, ordering)
+);
+COPY title.principals
+    FROM '/data/title.principals.tsv'
+    DELIMITER E'\t'
+    NULL '\N'
+    CSV HEADER;
+
+CREATE TABLE title.ratings (
+    tconst              TEXT        PRIMARY KEY,
+    average_rating      REAL,
+    num_votes           INT
+);
+COPY title.ratings
+    FROM '/data/title.ratings.tsv'
+    DELIMITER E'\t'
+    NULL '\N'
+    CSV HEADER;
+
+CREATE TABLE title.basics (
+    tconst              TEXT        PRIMARY KEY,
+    title_type          TEXT        NOT NULL,
+    primary_title       TEXT,
+    original_title      TEXT,
+    is_adult            INT,
+    start_year          INT,
+    end_year            INT,
+    runtime_minutes     INT,
+    genres              TEXT
+);
+COPY title.basics
+    FROM '/data/title.basics.tsv'
+    DELIMITER E'\t'
+    NULL '\N'
+    CSV HEADER;
